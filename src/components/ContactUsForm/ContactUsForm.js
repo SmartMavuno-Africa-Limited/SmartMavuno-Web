4a46eac095c06c038e7e5b691c25466f7eb215c1
Nelson-Tommogo <nelsontommogo9@gmail.com>
2024-11-10 08:02:24 +0300

    smartmavuno

--- src/components/ContactUsForm/ContactUsForm.js
+++ src/components/ContactUsForm/ContactUsForm.js
@@ -1,24 +1,43 @@
 import React, { useState } from "react";
 import styles from './ContactUsForm.module.css';
-// import {Map, GoogleApiWrapper} from 'google-maps-react';
 
-const ContactUsForm = () =>{
-    const [formContent, setFormContent ] = useState({});
+const ContactUsForm = () => {
+    const [formContent, setFormContent] = useState({
+        name: '',
+        email: '',
+        subject: '',
+        message: ''
+    });
 
-    const handleChange = ( event ) =>{
+    const handleChange = (event) => {
         const { value, name } = event.target;
-
-        setFormContent(prevState => ({
+        setFormContent((prevState) => ({
             ...prevState,
             [name]: value
-        }))
-    }
+        }));
+    };
 
+    const handleSubmit = () => {
+        if (!formContent.name || !formContent.email || !formContent.subject || !formContent.message) {
+            alert("Please fill out all the fields.");
+            return;  // Don't proceed if fields are missing
+        }
 
-    return(
+        // Construct the email content
+        const mailtoLink = `mailto:info@smartmavuno.africa?subject=${encodeURIComponent(formContent.subject)}&body=${encodeURIComponent(`
+            Name: ${formContent.name}\n
+            Email: ${formContent.email}\n
+            Message: ${formContent.message}
+        `)}`;
+
+        // Redirect to the email client with pre-filled subject and body
+        window.location.href = mailtoLink;
+    };
+
+    return (
         <>
             <div className={`container ${styles.contain} overflow-hidden`}>
-                <p data-aos='fade-up' className={`${styles.heading} mx-auto`}>We&apos;d Love to Hear From You</p>
+                <p data-aos='fade-up' className={`${styles.heading} mx-auto`}>Talk to us</p>
 
                 <div className="row">
                     <div className="col-md-4" data-aos='fade-right'>
@@ -28,56 +47,81 @@ const ContactUsForm = () =>{
 
                     <div className="col-md-4" data-aos='fade-up' data-aos-offset='100'>
                         <p className={styles.head}>Contact</p>
-                        <p className={styles.content}>Mobile 	: +254707687930</p> 
+                        <p className={styles.content}>Mobile: +254707687930</p>
                         <p className={styles.content}>Email: info@smartmavuno.africa</p>
                     </div>
 
                     <div className="col-md-4" data-aos='fade-left' data-aos-offset='70'>
-                        <p className={styles.head}>Address</p>
+                        <p className={styles.head}>Working Hours</p>
                         <p className={styles.content}>Monday - Friday: 09:00 - 20:00</p>
-                        <p className={styles.content}>Sunday &amp; Saturday: 10:30 - 22:00</p>
+                        <p className={styles.content}>Saturday & Sunday: 10:30 - 22:00</p>
                     </div>
                 </div>
 
                 <p className={styles.email} data-aos='fade-up'>You can email us</p>
 
                 <div className="row">
-                    <div className="col-md-6" data-aos='fade-right' style={{textAlign: 'left'}}>
-                        <label className={styles.label}>Name</label><br/>
-                        <input name="name" value={formContent.name} onChange={handleChange} className={styles.input} type="text" />
+                    <div className="col-md-6" data-aos='fade-right' style={{ textAlign: 'left' }}>
+                        <label className={styles.label}>Name</label><br />
+                        <input 
+                            name="name" 
+                            value={formContent.name} 
+                            onChange={handleChange} 
+                            className={styles.input} 
+                            type="text" 
+                        />
 
-                        <label className={styles.label}>Email</label><br/>
-                        <input name="email" value={formContent.email} onChange={handleChange} className={styles.input} type="email" />
+                        <label className={styles.label}>Email</label><br />
+                        <input 
+                            name="email" 
+                            value={formContent.email} 
+                            onChange={handleChange} 
+                            className={styles.input} 
+                            type="email" 
+                        />
 
-                        <label className={styles.label}>Subject</label><br/>
-                        <input name="subject" value={formContent.subject} onChange={handleChange} className={styles.input} type="text" />
+                        <label className={styles.label}>Subject</label><br />
+                        <input 
+                            name="subject" 
+                            value={formContent.subject} 
+                            onChange={handleChange} 
+                            className={styles.input} 
+                            type="text" 
+                        />
                     </div>
 
-                    <div className="col-md-6" data-aos='fade-left' style={{textAlign: 'left'}}>
-                        <label className={styles.label}>Message</label><br/>
-                        <textarea name="message" value={formContent.message} onChange={handleChange} className={`${styles.input} ${styles.msg}`} type="text"/>
-                        <div style={{textAlign:'right'}}>
-                            <button className={`btn custom_btn ${styles.btn}`}>SEND</button>
+                    <div className="col-md-6" data-aos='fade-left' style={{ textAlign: 'left' }}>
+                        <label className={styles.label}>Message</label><br />
+                        <textarea 
+                            name="message" 
+                            value={formContent.message} 
+                            onChange={handleChange} 
+                            className={`${styles.input} ${styles.msg}`} 
+                            type="text" 
+                        />
+
+                        {/* Optional ReCAPTCHA */}
+                        <div className="captcha-container">
+                            {/* Uncomment below if you want ReCAPTCHA but it's optional */}
+                            {/* <ReCAPTCHA 
+                                sitekey="your-site-key" 
+                                onChange={handleCaptchaChange} 
+                            /> */}
+                        </div>
+
+                        {/* Custom 'Send' Button */}
+                        <div style={{ textAlign: 'right' }}>
+                            <button 
+                                onClick={handleSubmit} 
+                                className={`${styles.btn} custom-btn`}>
+                                SEND
+                            </button>
                         </div>
                     </div>
                 </div>
             </div>
-
-            {/* <div>
-                <Map
-                    google={this.props.google}
-                    style={{width:'100%', height:'50%'}}
-                    zoom={14}
-                    initialCenter={
-                        {
-                            lat: 23.760374,
-                            lng: 90.411643
-                        }
-                    }
-                />
-            </div> */}
         </>
-    )
-}
+    );
+};
 
 export default ContactUsForm;
--- src/components/NavbarMain/NavbarMain.js
+++ src/components/NavbarMain/NavbarMain.js
@@ -1,17 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { Nav, Navbar, Container, NavDropdown } from "react-bootstrap";
-import { NavLink } from "react-router-dom";
-import { FaSignInAlt, FaUserPlus, FaHome } from 'react-icons/fa';
 import styles from "./NavbarMain.module.css";
-import logo from '../../assets/smart-mavuno-logo-zip-file/logo-color.png';  
+import { NavLink, Link } from "react-router-dom";
+import logo from '../../assets/smart-mavuno-logo-zip-file/logo-color.png';
 
 const NavbarMain = () => {
-  const [activeNav, setActiveNav] = useState([true, false, false, false, false, false]);
-  const [expand, setExpand] = useState(false);
-  const [showHomeDropdown, setShowHomeDropdown] = useState(false);
-  const [showResourcesDropdown, setShowResourcesDropdown] = useState(false);
-  const [showMoreInsightsDropdown, setShowMoreInsightsDropdown] = useState(false);
+  const [showServicesDropdown, setShowServicesDropdown] = useState(false);
   const [showRegistrationDropdown, setShowRegistrationDropdown] = useState(false);
+  const [showMoreInsightsDropdown, setShowMoreInsightsDropdown] = useState(false);
+  const [activeNav, setActiveNav] = useState([true, false, false, false, false]);
+  const [expand, setExpand] = useState(false);
 
   const closeNav = () => {
     setExpand(false);
@@ -54,92 +52,145 @@ const NavbarMain = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav style={{ marginLeft: "auto" }}>
-              {/* Home with dropdown for Market Place and Community */}
-              <NavDropdown
-                as={NavLink} // Make the title clickable (navigate to the home page)
-                to="/" // Link to the home page
-                title={<><FaHome /> Home</>}
-                id="home-dropdown"
-                className={`${styles.nav_text} nav-link`}
-                align="start"
-                show={showHomeDropdown}
-                onMouseEnter={() => setShowHomeDropdown(true)}
-                onMouseLeave={() => setShowHomeDropdown(false)}
+              <NavLink
+                to="/"
+                className={`${styles.nav_text} nav-link ${activeNav[0] ? styles.active : ""}`}
                 style={{ marginTop: "8px" }}
+                onClick={() => {
+                  handleActiveNav(0);
+                  closeNav();
+                }}
               >
-                {/* <NavDropdown.Item as={NavLink} to="/Services" onClick={() => { handleActiveNav(0); closeNav(); }}>
-                  Services
-                </NavDropdown.Item> */}
-                {/* <NavDropdown.Item as={NavLink} to="/community" onClick={() => { handleActiveNav(0); closeNav(); }}>
-                  Community
-                </NavDropdown.Item> */}
-                {/* <NavDropdown.Item as="a" href="https://smartmvua-forecast.netlify.app/" target="_blank" rel="noopener noreferrer" onClick={() => { handleActiveNav(0); closeNav(); }}>
-                  Weather Forecast
-                </NavDropdown.Item> */}
+                Home
+              </NavLink>
+
+              <NavDropdown
+                show={showServicesDropdown}
+                onMouseEnter={() => setShowServicesDropdown(true)}
+                onMouseLeave={() => setShowServicesDropdown(false)}
+                className={`nav-link ${styles.drop}`}
+                title={
+                  <Link
+                    to="/services"
+                    style={{ textDecoration: "none" }}
+                    className={styles.dropicon}
+                    onClick={() => {
+                      handleActiveNav(3);
+                      closeNav();
+                    }}
+                  >
+                    <span className={`${styles.nav_text} my-auto ${activeNav[3] ? styles.active : ""}`}>
+                      Resources
+                    </span>
+                  </Link>
+                }
+                id="basic-nav-dropdown"
+              >
+                <NavDropdown.Item className={styles.dropdownItem}>
+                  <NavLink
+                    to="/sMediaService"
+                    onClick={() => {
+                      handleActiveNav(3);
+                      closeNav();
+                    }}
+                    className={styles.dropdownText}
+                  >
+                    Projects & WorkShops
+                  </NavLink>
+                </NavDropdown.Item>
+                <NavDropdown.Item className={styles.dropdownItem}>
+                  <NavLink
+                    to="/modernfarming"
+                    onClick={() => {
+                      handleActiveNav(3);
+                      closeNav();
+                    }}
+                    className={styles.dropdownText}
+                  >
+                    Modern Farming
+                  </NavLink>
+                </NavDropdown.Item>
+                <NavDropdown.Item className={styles.dropdownItem}>
+                  <NavLink
+                    to="/blog"
+                    onClick={() => {
+                      handleActiveNav(3);
+                      closeNav();
+                    }}
+                    className={styles.dropdownText}
+                  >
+                    Articles & Blogs
+                  </NavLink>
+                </NavDropdown.Item>
               </NavDropdown>
 
-              {/* Resources dropdown with Projects, Workshops, Modern Farming, Articles/Blogs */}
+              {/* More Insights Dropdown with About Us and Contact Us */}
               <NavDropdown
-                as={NavLink} // Make the title clickable (navigate to /services)
-                to="/services" // Link to the services page
-                title="Resources"
-                id="resources-dropdown"
-                className={`${styles.nav_text} nav-link`}
-                align="start"
-                show={showResourcesDropdown}
-                onMouseEnter={() => setShowResourcesDropdown(true)}
-                onMouseLeave={() => setShowResourcesDropdown(false)}
-                style={{ marginTop: "8px" }}
-              >
-                <NavDropdown.Item as={NavLink} to="/projects" onClick={() => { handleActiveNav(3); closeNav(); }}>
-                  Projects
-                </NavDropdown.Item>
-                <NavDropdown.Item as={NavLink} to="/workshops" onClick={() => { handleActiveNav(3); closeNav(); }}>
-                  Workshops
-                </NavDropdown.Item>
-                <NavDropdown.Item as={NavLink} to="/modern-farming" onClick={() => { handleActiveNav(3); closeNav(); }}>
-                  Modern Farming
-                </NavDropdown.Item>
-                <NavDropdown.Item as={NavLink} to="/blogs" onClick={() => { handleActiveNav(3); closeNav(); }}>
-                  Articles/Blogs
-                </NavDropdown.Item>
-              </NavDropdown>
-
-              {/* More Insights dropdown with About Us and Contact Us */}
-              <NavDropdown
-                title="More Insights"
-                id="more-insights-dropdown"
-                className={`${styles.nav_text} nav-link`}
-                align="start"
                 show={showMoreInsightsDropdown}
                 onMouseEnter={() => setShowMoreInsightsDropdown(true)}
                 onMouseLeave={() => setShowMoreInsightsDropdown(false)}
-                style={{ marginTop: "8px" }}
+                className={`${styles.nav_text} nav-link`}
+                title="More Insights"
+                id="more-insights-dropdown"
               >
-                <NavDropdown.Item as={NavLink} to="/aboutUs" onClick={() => { handleActiveNav(4); closeNav(); }}>
-                  About Us
+                <NavDropdown.Item>
+                  <NavLink
+                    to="/aboutUs"
+                    className={styles.dropdownText}
+                    onClick={() => {
+                      handleActiveNav(4);
+                      closeNav();
+                    }}
+                  >
+                    About Us
+                  </NavLink>
                 </NavDropdown.Item>
-                <NavDropdown.Item as={NavLink} to="/contactUs" onClick={() => { handleActiveNav(4); closeNav(); }}>
-                  Contact Us
+                <NavDropdown.Item>
+                  <NavLink
+                    to="/contactUs"
+                    className={styles.dropdownText}
+                    onClick={() => {
+                      handleActiveNav(4);
+                      closeNav();
+                    }}
+                  >
+                    Contact Us
+                  </NavLink>
                 </NavDropdown.Item>
               </NavDropdown>
 
-              {/* Registration dropdown with SignUp and Login */}
+              {/* Registration Dropdown */}
               <NavDropdown
-                title="Registration"
-                id="registration-dropdown"
-                className={`${styles.nav_text} nav-link`}
-                align="start"
                 show={showRegistrationDropdown}
                 onMouseEnter={() => setShowRegistrationDropdown(true)}
                 onMouseLeave={() => setShowRegistrationDropdown(false)}
-                style={{ marginTop: "8px" }}
+                className={`${styles.nav_text} nav-link`}
+                title="Registration"
+                id="nav-registration-dropdown"
               >
-                <NavDropdown.Item as={NavLink} to="/signup" onClick={() => { handleActiveNav(5); closeNav(); }}>
-                  <FaUserPlus /> Sign Up
+                <NavDropdown.Item>
+                  <NavLink
+                    to="/signup"
+                    className={styles.dropdownText}
+                    onClick={() => {
+                      handleActiveNav(5);
+                      closeNav();
+                    }}
+                  >
+                    Signup
+                  </NavLink>
                 </NavDropdown.Item>
-                <NavDropdown.Item as={NavLink} to="/login" onClick={() => { handleActiveNav(5); closeNav(); }}>
-                  <FaSignInAlt /> Login
+                <NavDropdown.Item>
+                  <NavLink
+                    to="/login"
+                    className={styles.dropdownText}
+                    onClick={() => {
+                      handleActiveNav(5);
+                      closeNav();
+                    }}
+                  >
+                    Login
+                  </NavLink>
                 </NavDropdown.Item>
               </NavDropdown>
             </Nav>
